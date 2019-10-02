8f862ee83a51ee012c6e23974d6ccbaac97d98e2
Luciano <luciano.9912@gmail.com>
2019-10-02 17:38:03 -0300

    Maps

--- js/gasolina.js
+++ js/gasolina.js
@@ -59,16 +59,59 @@ function makeCards(cardsArea, value) {
                 <h3 class="card-title text-center">R$${value.preco}</h3>
             </div>
         </div>
-        <div>
-            <h6 class="card-title">${value.fornecedor}</h6>
-            <small>R$ ${value.diferenca_menor_maior} mais barato</small>
+        <div class="container">
+            <div class="row">
+                <div class="col-8">
+                    <div>
+                        <h6 class="card-title">${value.fornecedor}</h6>
+                        <small>R$ ${value.diferenca_menor_maior} mais barato</small>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <button type="button" class="btn btn-outline-light" 
+                    data-toggle="modal" data-target="#myModal" data-lat='10.85' data-lng='106.62'>
+                        <i class="fas fa-map-marked-alt"></i>
+                    </button>
+                </div>
+            </div>
         </div>
     </div>
 </div>
 </div>
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog  modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="col-md-12 modal_body_content">
+            <p>Some contents...</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 modal_body_map">
+            <div class="location-map" id="location-map">
+              <div style="width: 600px; height: 400px;" id="map_canvas"></div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 modal_body_end">
+            <p>Else...</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 `
     $(cardsArea).append(tag);
 }
+
 
 async function makeCharts(diesel, gasolina, etanol) {
     let chartD = {}
