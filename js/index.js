a910ea68fd559c32cc7ba5943eb08a3f7133cae7
Luciano <luciano.9912@gmail.com>
2019-10-02 15:24:16 -0300

    Mudanças no modal

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ function btnArea(){
     <i class="far fa-comment-alt"></i>
   </button>
   <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
+      <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">Dúvidas, criticas e sugestões</h5>
@@ -26,7 +26,7 @@ function btnArea(){
             </form>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
             <button type="button" class="btn btn-primary">Enviar</button>
           </div>
         </div>
