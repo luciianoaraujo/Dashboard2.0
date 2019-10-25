7e805993fd502a073e0d48a645f29b8f735852e2
Pbenteu <39163104+Pbenteu@users.noreply.github.com>
2019-10-25 15:25:00 -0300

    Update maps nos modais

--- js/gasolina.js
+++ js/gasolina.js
@@ -6,6 +6,12 @@ $('#escolha').on('click', (e) => {
     $('#escolhaInfo').toggleClass('hide');
         
 })
+let scriptString = `
+function drawMap() {    
+    var mapOptions = {
+      zoom: 13,
+     }
+ `;
 
 async function loadTableData(query) {
     var url = `http://localhost:3333/${query}`;
@@ -48,6 +54,9 @@ function makeTable(tableName, value) {
 }
 
 function makeCards(cardsArea, value) {
+    let fornecedorId = value.fornecedorId;
+    let latitude = value.latitude;
+    let longitude = value.longitude;
     let tag = `
 <div class="card2">
 <div class="card mb-4 cardcom" id='${value.nome}Card'>
@@ -70,7 +79,7 @@ function makeCards(cardsArea, value) {
                 </div>
                 <div class="col-4">
                     <button type="button" class="btn btn-outline-light" 
-                    data-toggle="modal" data-target="#myModal" data-lat='10.85' data-lng='106.62'>
+                    data-toggle="modal" data-target="#myModal${value.id}" data-lat='10.85' data-lng='106.62'>
                         <i class="fas fa-map-marked-alt"></i>
                     </button>
                 </div>
@@ -79,38 +88,55 @@ function makeCards(cardsArea, value) {
     </div>
 </div>
 </div>
-<!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal fade" id="myModal${value.id}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog  modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
+        <h4 class="modal-title" id="myModalLabel">${value.fornecedor}</h4>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
       </div>
       <div class="modal-body">
         <div class="row">
           <div class="col-md-12 modal_body_content">
-            <p>Some contents...</p>
           </div>
         </div>
         <div class="row">
-          <div class="col-md-12 modal_body_map">
-            <div class="location-map" id="location-map">
-              <div style="width: 600px; height: 400px;" id="map_canvas"></div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12 modal_body_end">
-            <p>Else...</p>
-          </div>
+          <div id="map${value.id}" style="height:500px; width:100%"></div>
         </div>
       </div>
     </div>
   </div>
 </div>
-`
-    $(cardsArea).append(tag);
+`;
+scriptString = scriptString+`
+    map${value.id} = new google.maps.Map(document.getElementById("map${value.id}"), {
+        center: {lat: ${value.latitude}, lng: ${value.longitude}},
+        zoom: 18,
+    });
+    var marker = new google.maps.Marker({
+        position: {lat: ${value.latitude}, lng: ${value.longitude}}, 
+        map: map${value.id},
+        title: 'Hello World!'
+    });
+`;
+if(value.id==24){
+    scriptString = scriptString+`}
+    `;
+
+    console.log(scriptString);
+
+    var googleMapsScript = document.createElement('script');
+    scriptString = document.createTextNode(scriptString);
+    googleMapsScript.appendChild(scriptString);
+    document.body.appendChild(googleMapsScript);
+
+    var my_awesome_script = document.createElement('script');
+    my_awesome_script.setAttribute('src',`https://maps.googleapis.com/maps/api/js?key=AIzaSyBCnuLREFEtwCwMuLmAG3Mmre7uOXe7g40&callback=drawMap`);
+    document.body.appendChild(my_awesome_script);
+
+}
+
+    $(cardsArea).append(tag)
 }
 
 
